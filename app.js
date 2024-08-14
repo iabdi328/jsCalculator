01f4e537f077a56242a51a188c3bc994212ea946
iabdi328 <ismailabdi328@gmail.com>
2024-08-13 22:46:26 -0400

    working js calculator completed

---
+++ app.js
@@ -0,0 +1,124 @@
+const display = document.querySelector(".displaybox");
+display.innerHTML = " <p></p>"
+
+const main = document.querySelector(".main")
+
+let currentInput = "";
+let operation = "";
+let firstNumber = "";
+let secondNumber = "";
+
+
+const updateDisplay = (content) => {
+    display.innerHTML = `<p>${content}</p>`;
+}
+
+
+main.addEventListener("click", (e) => {
+    const button = e.target;
+    const value = button.textContent;
+
+
+    if (button.tagName === "BUTTON"){
+        let currentInput = value;
+        display.innerHTML += currentInput;
+        
+
+        if (currentInput === "AC"){
+            display.innerHTML = ""
+            currentInput = ""
+            firstNumber = "";
+            secondNumber = "";
+            operation = "";
+        }
+
+
+        if (operation === "") {
+            if (currentInput === "."){
+
+                if (!firstNumber.includes(".")){
+                    firstNumber += ".";
+                } 
+            }
+            if (!firstNumber && ["+", "-", "*", "/"].includes(currentInput)) {
+                operation = currentInput; 
+            }
+
+            else if (!isNaN(parseFloat(currentInput)) && isFinite(currentInput)) {
+               
+                firstNumber += currentInput;
+                
+            }
+        }
+        if  (firstNumber !== "" && ["+", "-", "*", "/"].includes(currentInput)){
+            operation += currentInput;
+            updateDisplay(`${firstNumber} ${operation}`);
+        }
+
+
+        if(operation){
+            if (currentInput === "."){
+                if (!secondNumber.includes(".")){
+                    secondNumber += ".";
+                }
+
+            }
+            else if (!isNaN(parseFloat(currentInput)) && isFinite(currentInput)) {
+                secondNumber += currentInput;
+            }
+
+
+        }
+
+
+
+
+        if (currentInput === "="){
+            if (firstNumber !== "" && secondNumber !== "" && operation !== ""){
+                if (operation == "+"){
+                    updateDisplay(add(firstNumber, secondNumber))
+                }
+                else if (operation == "-"){
+                    updateDisplay(sub(firstNumber, secondNumber))
+                }
+                else if (operation == "*"){
+                    updateDisplay(multiply(firstNumber, secondNumber))
+                }
+                else if (operation == "/"){
+                    updateDisplay(divide(firstNumber, secondNumber))
+                }
+            }
+            
+        }
+
+    }
+    
+});
+
+
+
+
+
+function add (num1, num2){
+    return Number(num1) + Number(num2);
+};
+
+
+function subtract (num1, num2){
+    return Number(num1) - Number(num2);
+};
+
+
+
+function multiply (num1, num2){
+    return Number(num1) * Number(num2);
+};
+
+
+
+function divide (num1, num2){
+    if (Number(num2) === 0){
+        return "Error"
+    }
+    return Number(num1) / Number(num2);
+};
